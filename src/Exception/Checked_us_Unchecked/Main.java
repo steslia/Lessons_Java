1519682de04a878a0039e83e979b343b12bf9baf
Sergey Teslya <teslya147@gmail.com>
2019-05-10 00:57:52 +0300

    Lessons Exeption 2

--- src/Exception/Checked_us_Unchecked/Main.java
+++ src/Exception/Checked_us_Unchecked/Main.java
@@ -1,11 +1,49 @@
 package Exception.Checked_us_Unchecked;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test git commit");
-        System.out.println("test teest git");
+    //Добавляем изключение в сигнатуру главного метода
+    //И обрабатываем такие исключения в верх по иерархии метода
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueLoop = true;
+        PrintWriter writer = null;
+        do {
+            try {
+                System.out.println("Please enter numerator!");
+                int numerator = scanner.nextInt();
+                System.out.println("Please enter denominator!");
+                int denominator = scanner.nextInt();
+                System.out.println(divide(numerator, denominator));
+                writer = new PrintWriter(new FileWriter("out.txt"));
+                writer.println("Result = "+divide(numerator, denominator));
+                continueLoop = false;
 
-        System.out.println("New branch Fix");
+            }
+/*            catch (ArithmeticException | InputMismatchException e) {
+                System.out.println("Exception " + e);
+                scanner.nextLine();
+                System.out.println("Параметр int для деления не должен быть 0");
+            } catch (IOException e) {
+                System.out.println("Файл открыт");
+                e.printStackTrace();
+            } */
+            finally {
+                //В этот блок помешаем код который обезательно должен выполница
+                //В этом блоке код выполнитца даже если блок try выполнился не успешно
+                System.out.println("Finally блок запустился");
+                writer.close();
 
+            }
+        } while (continueLoop);
+    }
+
+    //Декларируем что метод выбрасывает Exception
+    private static int divide(int numerator, int denominator) throws ArithmeticException, NullPointerException {
+        return numerator / denominator;
     }
 }
